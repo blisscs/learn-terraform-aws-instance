b1cb4a354c6b3e1a9194554e575a0fb8384703f2
Suracheth Chawla <dev1987@gmail.com>
2022-02-10 23:13:17 +0700

    edit the configuration and apply

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ terraform {
 
 provider "aws" {
   profile = "default"
-  region  = "ap-northeast-1"
+  region  = "us-west-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-088da9557aae42f39"
+  ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 
   tags = {
